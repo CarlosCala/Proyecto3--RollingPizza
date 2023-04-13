3e649a9e3f32dcb29cd3e2909edb0e08f874f228
CarlosCala <carlosdev52@gmail.com>
2023-04-13 20:22:35 -0300

    corrections 404

--- src/views/layouts/Error404.js
+++ src/views/layouts/Error404.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const Error404 = () => {
     return (
-        <div className='text-center'>
-            <img alt="imagen error 404"  className='rounded' src="https://img.freepik.com/vector-premium/pizza-estado-vacio-error-404-ilustracion-plana_288067-137.jpg?w=740"></img>
+        <Container className='error404'>
+        <div className='text-center '>
+            <img alt="imagen error 404"  className='img-fluid' src="https://img.freepik.com/vector-premium/pizza-estado-vacio-error-404-ilustracion-plana_288067-137.jpg?w=740"></img>
         </div>
+        </Container>
     );
 };
 
