3e43269b0c6a2a62f46e09f82ef7b6e03e0f3131
CarlosCala <carlosdev52@gmail.com>
2023-04-13 14:14:34 -0300

    if you are not logged in, you will be taken to the login page

--- src/views/home/cardProduct/CardProduct.js
+++ src/views/home/cardProduct/CardProduct.js
@@ -3,13 +3,17 @@ import { Card, Col, Row } from "react-bootstrap";
 // import { Next } from "react-bootstrap/esm/PageItem";
 import { Link, useNavigate } from "react-router-dom";
 
-const CardProduct = ({ product}) => {
+const CardProduct = ({ product, loggedUser }) => {
 
 
   return (
-    <div >
+    <div>
       <Card className="my-4 cardProduct border border-ligth cardsProducts">
-        <Card.Img className="img-fluid cardImg" variant="top" src={product.urlImg} />
+        <Card.Img
+          className="img-fluid cardImg"
+          variant="top"
+          src={product.urlImg}
+        />
         <Card.Body>
           <div className="d-flex align-items-center justify-content-between mb-2">
             <Card.Title className="m-0 text-truncate">
@@ -19,12 +23,22 @@ const CardProduct = ({ product}) => {
           </div>
           <div className="d-flex align-items-center justify-content-between">
             <p className="mb-0 ms-4 fs-4 ">${product.price}</p>
-            <Link
-              to={`/product/buy/${product._id}`}
-              className="btn-gray text-decoration-none text-center"
-            >
-              Buy
-            </Link>
+
+            {loggedUser && Object.keys(loggedUser).length > 0 ? (
+              <Link
+                to={`/product/buy/${product._id}`}
+                className="btn-gray text-decoration-none text-center"
+              >
+                Buy
+              </Link>
+            ) : (
+              <Link
+                to="auth/login/"
+                className="btn-gray text-decoration-none text-center"
+              >
+                Buy
+              </Link>
+            )}
           </div>
         </Card.Body>
       </Card>
